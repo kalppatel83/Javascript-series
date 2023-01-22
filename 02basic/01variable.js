ef12d6ad0b2aed9fc6f578bc070f061fc5c716fe
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-22 23:05:30 +0530

    about variables

---
+++ 02basic/01variable.js
@@ -0,0 +1,12 @@
+// Declaring the variable and SomeData type intro
+
+var fullName = "Kalp Patel";
+var courseName = "ReactJs Bootcamp";
+var isLoggedIn = true;
+
+var loggedCount = 34;
+
+var paymentMode;
+console.log(paymentMode);
+paymentMode = "Debit Card";
+console.log(paymentMode);
