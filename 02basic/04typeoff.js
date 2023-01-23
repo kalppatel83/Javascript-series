c75cc34c44f28e0404ac3ba88883e4cea0aab55b
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-23 10:00:01 +0530

    typeof-operator

---
+++ 02basic/04typeoff.js
@@ -0,0 +1,16 @@
+var username = "Kalp";
+var contactNo = 123456;
+
+var a = 8;
+var b = 10;
+var sum = a > b;
+
+var allUsers = ["Ton", "Tod", "Sod"];
+
+console.log(typeof username);
+console.log(typeof contactNo);
+
+console.log(typeof sum);
+console.log(typeof allUsers); //object
+
+// TODO: Assignment read on operator precedence
