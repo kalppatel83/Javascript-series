d4621dd6ff5e3f9afd3f392e362fd8446eff74f9
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-23 10:22:19 +0530

    TempeatureApp-using-if&else

---
+++ 02basic/05Conditional.js
@@ -0,0 +1,24 @@
+var temperature = 5;
+
+// If & else works on boolean value if it's true then the block of code will execute if
+// false it will check for other codition if available or it will execute the else block
+
+if (temperature <= 20) {
+  console.log(`It's cold outside`);
+} else if (temperature <= 30) {
+  console.log(`It's Modrate outside`);
+} else {
+  console.log(`It's hold outside`);
+}
+
+// TODO: Some details
+// If & else works on boolean value if it's true then the block of code will execute if
+// false it will check for other codition if available or it will execute the else block
+// 1 -> true
+// 0 -> false
+if (1) {
+  console.log("hello");
+}
+if (0) {
+  console.log("This will not be run");
+}
