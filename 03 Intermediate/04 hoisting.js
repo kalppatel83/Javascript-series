1ac4e196bf254ec3cd0eb767dac277b961982c5c
Kalp Patel <kalp_patel@rocketmail.com>
2023-01-25 07:06:31 +0530

    hoisting part done

---
+++ 03 Intermediate/04 hoisting.js
@@ -0,0 +1,23 @@
+tipper(5);
+function tipper(a) {
+  var bill = parseInt(a);
+  console.log(bill + 5);
+}
+
+var bigTipper = function (a) {
+  var bill = parseInt(a);
+  console.log(bill + 15);
+};
+bigTipper("225");
+
+console.log(name);
+var name = "kalp";
+
+function sayName() {
+  var name = "Mr.H";
+  console.log(name);
+}
+sayName();
+//bcoz it will not dig below into code and work for his work only and will go away
+
+console.log(name);
