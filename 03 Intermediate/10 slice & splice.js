97bc2d2aa6eb7996600783a77d21b838f0bdb05f
Kalp Patel <kalp_patel@rocketmail.com>
2023-04-01 22:54:15 +0530

    slice&splice

---
+++ 03 Intermediate/10 slice & splice.js
@@ -0,0 +1,6 @@
+var users = ["Ted", "Tim", "Ton", "Sam", "Sor", "Sod"];
+
+// ("Start position => inclusive", end position => exclusive)
+console.log(users.slice(1, 3));
+// or
+console.log(users.slice(1));
